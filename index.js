04498eb3273c4ae4e5d8d177e2ac78ed91db3b72
Umer <omerjh5004@gmail.com>
2025-04-20 23:20:50 +0500

    Changes

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Nodemon-powered Node.js project!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
