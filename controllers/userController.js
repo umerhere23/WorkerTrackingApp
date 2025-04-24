489d3eeba1db2446286174a1fbf60fd0a68ce741
Umer <omerjh5004@gmail.com>
2025-04-24 14:41:36 +0500

    updates

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,7 +57,7 @@ exports.getUserById = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
-
+ 
     res.status(200).json(user);
   } catch (error) {
     console.error("Error fetching user:", error);
